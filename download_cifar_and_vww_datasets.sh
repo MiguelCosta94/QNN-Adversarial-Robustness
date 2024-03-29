898bf540139847c29d565b1d611aea8c06762ba5
Miguel <miguelcosta_mapc@live.com.pt>
2024-03-29 22:16:49 +0000

    add link to coffee dataset

--- download_cifar_and_vww_datasets.sh
+++ download_cifar_and_vww_datasets.sh
@@ -5,5 +5,3 @@ wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
 tar -xvf cifar-10-python.tar.gz
 wget https://www.silabs.com/public/files/github/machine_learning/benchmarks/datasets/vw_coco2014_96.tar.gz
 tar -xvf vw_coco2014_96.tar.gz
-wget https://mega.nz/file/ufg2jISB#CA1ssv9fUozs3_dsfDz4PI8fNE0K-_eXYb9Uf79ShIU
-tar -xvf new_coffee_dataset.tar.xz
